b24961de23155f4cf8a4febf98ebb4145487e403
raihanmakarim <raihanmakarim21@gmail.com>
2022-11-11 21:17:27 +0700

    monyet

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ const Page = async () => {
   return (
     <div className="flex">
       <div className="text-center text-green-500">
-        sadasdasdasdasdasdasdsaasdadasdasd
+        sadasdasdasdasdasdasdsaasdadasdasdasdasdasdsa monyet
       </div>
       <div>asdasd</div>
     </div>
